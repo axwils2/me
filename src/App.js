a7bc479bf207d0937f577ce8d60715028ec20496
Austin Wilson <austin@newcoastventures.com>
2019-07-05 13:51:35 -0500

    create basic nav directory

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
-import logo from './assets/logo.svg';
 import './App.css';
+
+import Nav from './Nav';
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Oh hey sabrina and marie</p>
-      </header>
+      <Nav />
     </div>
   );
 }
---
+++ src/Nav/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../assets/logo.svg';
+
+const Nav = () => (
+  <nav>
+    <img src={logo} alt="logo" />
+    <div>About</div>
+    <div>Clients</div>
+    <div>Contact</div>
+  </nav>
+)
+
+export default Nav;
