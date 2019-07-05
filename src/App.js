435fa5806fc308580ca42de20659285ba55100bd
Austin Wilson <austin@newcoastventures.com>
2019-07-05 13:47:19 -0500

    add hot reloading

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
+import { hot } from 'react-hot-loader/root';
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Oh hey sabrina and marie</p>
       </header>
     </div>
   );
 }
 
-export default App;
+export default hot(App);
